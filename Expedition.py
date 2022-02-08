41e51d13c6a9c71f54f7f3e7a290ac42072cb517
ArnouxE <78238358+ArnouxE@users.noreply.github.com>
2022-02-08 11:56:22 +0100

    On as reussi a afficher un truc GG

--- Expedition.py
+++ Expedition.py
@@ -1,6 +1,58 @@
+import pygame
+import random
+
 class Expedition():
-    def __init__(self):
+    def __init__(self,game):
         self.strat = 0
         self.map_name = ""
-        self.length = 1
+        self.length =  random.randint(1, 8)
         self.layout = None
+        self.game = game
+        self.mid_w, self.mid_h = self.game.WIDTH /2, self.game.HEIGHT /2 
+        self.run_display = True
+
+    def blit_screen(self):
+        self.game.window.blit(self.game.display,(0, 0))
+        pygame.display.update()
+
+class EventOre(Expedition):
+    def __init__(self, game):
+        Expedition.__init__(self,game)
+
+
+    def Display_ore(self):
+        print("affiche event ore")
+        self.run_display = True
+        while self.run_display:
+            self.game.display.fill(self.game.BLACK)
+            self.game.draw_text("ore event",20,self.mid_w,self.mid_h)
+            self.blit_screen()
+
+
+class EventGaz(Expedition):
+    def __init__(self, game):
+        Expedition.__init__(self,game)
+
+
+    def Display_Gaz(self):
+        print("affiche event gaz")
+        self.run_display = True
+        while self.run_display:
+            self.game.display.fill(self.game.BLACK)
+            self.game.draw_text("gaz event",20,self.mid_w,self.mid_h)
+            self.blit_screen()
+
+
+class EventRituel(Expedition):
+    def __init__(self, game):
+        Expedition.__init__(self,game)
+
+
+    def Display_rituel(self):
+        print("affiche event rituel")
+        self.run_display = True
+        while self.run_display:
+            self.game.display.fill(self.game.BLACK)
+            self.game.draw_text("rituel event",20,self.mid_w,self.mid_h)
+            self.blit_screen()
+
--- Game.py
+++ Game.py
@@ -1,3 +1,6 @@
+from multiprocessing.connection import wait
+import this
+from time import sleep
 import pygame
 from Farm import Farm
 
@@ -13,14 +16,20 @@ class Game:
         self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
         self.font_name = pygame.font.get_default_font()
         self.BLACK, self.WHITE = (0, 0, 0), (255, 255, 255)
+        
 
     def game_loop(self):
         while self.playing:
             self.check_events()
+            self.display.fill(self.BLACK)
+            self.draw_text("cock Digger",100,self.WIDTH/2,self.HEIGHT/2,self.WHITE)
+            self.screen.blit(self.display,(0,0))
+            pygame.display.update()
 
-    def draw_text(self, text, size, x, y, color):
+
+    def draw_text(self, text, size, x, y,color):
         font = pygame.font.Font(self.font_name, size)
-        text_surface = font.render(text, True, color)
+        text_surface = font.render(text, True,color)
         text_rect = text_surface.get_rect()
         text_rect.center = (x, y)
         self.display.blit(text_surface, text_rect)

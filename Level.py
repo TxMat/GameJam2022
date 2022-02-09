84d513e5a114ddfd5449139b7a01b8c1b309cea9
ArnouxE <78238358+ArnouxE@users.noreply.github.com>
2022-02-09 12:21:56 +0100

    ajout de gen_level

--- Level.py
+++ Level.py
@@ -1,3 +1,4 @@
+from os import name
 from random import randint
 
 class Level():
@@ -25,3 +26,11 @@ class Level():
 
     def gen_len(self):
         return randint(self.len_range(0), self.len_range(1))
+
+    def gen_level():
+        level = {}
+        level ["mine d'argent"] = Level(name="mine d'argent", ore_list=["cuivre","argent","émeraude"] ,dna_list=["h1n1","b5r3","d1c3"],description="une mine d'argent, rien de plus... rien de moins",danger_lvl=1,length_range=(3,8))
+        level ["mine dangereuse"] = Level(name="mine dangereuse", ore_list=["Columbite","granit","saphir"] ,dna_list=["n1g6","i8a5","z9g1"],description="une mine pleine de ressources",danger_lvl=3,length_range=(3,15))
+        level ["mine d'or"] = Level(name="mine d'or", ore_list=["cuivre","or","émeraude"] ,dna_list=["l5z5","b1t3","u8x1"],description="Sûrment une des plus vielle que la terre est portée",danger_lvl=2,length_range=(10,15))        
+        level ["mine de diamant"] = Level(name="mine de diamant", ore_list=["fer","platine","diamant"] ,dna_list=["y2w3","c0c5","e9y5"],description="elle brille meme de l'exterieur",danger_lvl=13,length_range=(3,15))
+        level ["mine de l'extreme"] = Level(name="mine de l'extreme", ore_list=["sable argileux","plomb","francium"] ,dna_list=["e8t1","h8k3","i2y4"],description="vous etes sûr de vouloir renter",danger_lvl=67,length_range=(3,15))

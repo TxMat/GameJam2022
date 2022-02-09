087c5b4a02724ce1084b0b50c9deb4250ffcc321
ponalm <mathieu.ponal@etu.univ-grenoble-alpes.fr>
2022-02-09 20:24:52 +0100

    reformatting

--- CockList.py
+++ CockList.py
@@ -9,7 +9,7 @@ class CockList(State):
         self.player = player
         self.background_img = pygame.image.load("Assets/menubg.png")
         self.background_rect = self.background_img.get_rect()
-        self.background_rect.center = (self.game.WIDTH/2, self.game.HEIGHT/2)
+        self.background_rect.center = (self.game.WIDTH / 2, self.game.HEIGHT / 2)
 
     def update(self, delta_time, actions):
         if actions["esc"]:
@@ -20,3 +20,4 @@ class CockList(State):
         self.prev_state.render(surface)
         surface.fill((100, 100, 100, 100), None, pygame.BLEND_RGBA_MULT)
         surface.blit(self.background_img, self.background_rect)
+

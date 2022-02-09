dfc19b55dde0f45e54e70f137344e7a5cdb806cf
carrel <dvquentincarrel@gmail.com>
2022-02-09 10:13:30 +0100

    bugfix + added tests

--- Grain.py
+++ Grain.py
@@ -1,5 +1,14 @@
 class Grain():
-    def __init__(self, name = "", price = 0, res_cost = 0, res_mat = "", hunger = 1, int_b = 0, sta_b = 0, str_b = 0, unlocked = False):
+    def __init__(self,
+            name = "",
+            price = 0,
+            res_cost = 0,
+            res_mat = "",
+            hunger = 1,
+            int_b = 0,
+            sta_b = 0,
+            str_b = 0,
+            unlocked = False):
         self.name = name
         self.price = price
         self.res_cost = res_cost
@@ -10,23 +19,23 @@ class Grain():
         self.str_bonus = str_b
         self.is_unlocked = unlocked
 
-def disp_debug(grain):
-        print("name: " + grain.name)
-        print("price: " + str(grain.price))
-        print("r_cost: " + str(grain.res_cost))
-        print("r_mat: " + grain.res_mat)
-        print("hunger: " + str(grain.hunger))
-        print("int bonus: " + str(grain.int_bonus))
-        print("sta bonus: " + str(grain.sta_bonus))
-        print("str bonus: " + str(grain.str_bonus))
-        print("unlocked : " + str(grain.is_unlocked))
+    def grain_info(self):
+            print("name: " + self.name)
+            print("price: " + str(self.price))
+            print("r_cost: " + str(self.res_cost))
+            print("r_mat: " + self.res_mat)
+            print("hunger: " + str(self.hunger))
+            print("int bonus: " + str(self.int_bonus))
+            print("sta bonus: " + str(self.sta_bonus))
+            print("str bonus: " + str(self.str_bonus))
+            print("unlocked : " + str(self.is_unlocked))
 
-from random import randint
+from random import randint, choice
 
-def test_grain(nb = 1) -> [Grain]:
+def debug_grain(nb = 1) -> [Grain]:
     grains = []
     for i in range(nb):
-        grains.append(Grain("Grain " + str(i+1), randint(100, 5000), randint(1, 50), "ADN " + str(randint(1, 10)), randint(1,5), randint(1,5), randint(1,5), randint(1,5)))
-        disp_debug(grains[i])
+        grains.append(Grain("Grain " + str(i+1), randint(100, 5000), randint(1, 50), "ADN " + str(randint(1, 10)), randint(1,5), randint(1,5), randint(1,5), randint(1,5), choice((True, False))))
+        grains[i].grain_info()
         print("")
     return grains
--- tests.py
+++ tests.py
@@ -9,20 +9,67 @@ from Perks import *
 from Player import *
 from Tree import *
 
-print("Creation coq 1\n")
-cock_a = Cock(1,"coq 1")
-print("Infos coq 1\n")
-cock_a.info_cock()
-print("")
-print("Ponte coq 2 par coq 1\n")
-cock_b = cock_a.lay_egg(2,"coq 2")
-print("Fertilité coq 1: " + str(cock_a.fertile))
-print("Id enfant coq 1: " + str(cock_a.child))
-print("Infos coq 2\n")
-cock_b.info_cock()
+def test_cock():
+    print("Creation coq 1\n")
+    cock_a = Cock(1,"coq 1")
+    print("Infos coq 1\n")
+    cock_a.info_cock()
+    print("")
+    print("Ponte coq 2 par coq 1\n")
+    cock_b = cock_a.lay_egg(2,"coq 2")
+    print("Fertilité coq 1: " + str(cock_a.fertile))
+    print("Id enfant coq 1: " + str(cock_a.child))
+    print("Infos coq 2\n")
+    cock_b.info_cock()
+    return (cock_a, cock_b)
+
+def test_grain():
+    grains = debug_grain(5) 
+    return grains
+
+def test_perk():
+    class PerkFireproof(Perks):
+        def __init__(self,
+            name: str ,
+            icon = None,
+            tier: int = 1,
+            description: str = "",
+            int_mod: int = 0,
+            int_mult: float = 1,
+            str_mod: int = 5,
+            str_mult: float = 5000,
+            sta_mod: int = 0,
+            sta_mult: float = 1):
+            super().__init__(name, icon, tier, description, int_mod, int_mult, str_mod, str_mult, sta_mod, sta_mult)    
+
+    def action(self, cock: Cock) -> None:
+        #doesn't work
+        cock.strength += 50000
+    fireproof = PerkFireproof("fireproof")
+    perks = [fireproof]
+    for i in range(2,15):
+        perks.append(Perks("Perk " + str(i), None, 0, "", randint(0,50), randint(0,50), randint(0,50), randint(0,50), randint(0,50), randint(0,50)))
+    perks[1].tier = 2
+    perks[2].tier = 2
+    perks[3].tier = 2
+    perks[4].tier = 3
+    perks[5].tier = 3
+    perks[6].tier = 3
+    perks[7].tier = 4
+    perks[8].tier = 4
+    perks[9].tier = 4
+    perks[10].tier = 4
+    perks[11].tier = 4
+    perks[12].tier = 5
+    perks[13].tier = 5
+    return perks
+
+a = test_cock()
+b = test_grain()
+c = test_perk()
+a[0].add_perk("fireproof",{"fireproof":c[0]})
 
 print("TODO: \n\
-    dummy grain\n\
     dummy events\n\
     dummy perks\n\
     dummy levels\n\
